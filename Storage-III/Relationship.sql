c9700b4025e777e42b8189e4483b73fa5f9d5bc1
Evelyn Cristini Oliveira <evelyn.oliveira@mercadolivre.com>
2022-07-26 15:27:31 -0300

    Add script de banco de dados para geração de relacionamentos

---
+++ Storage-III/Relationship.sql
@@ -0,0 +1,14 @@
+use bdmeli02;
+
+insert into author values (null, 'Author 1');
+insert into author values (null, 'Author 2');
+
+insert into address values (null, 'Endereco 1', 1);
+insert into address values (null, 'Endereco 2', 2);
+
+insert into subject values (null, 'Assunto 1');
+insert into subject values (null, 'Assunto 2');
+
+insert into book values (null, 'Livro 1', 1);
+insert into book values (null, 'Livro 2', 1);
+insert into book values (null, 'Livro 3', 2);
